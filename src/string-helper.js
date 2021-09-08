8e471bde340ac49fcfcb38310202c0d695b35ea9
Pieter Wigboldus <w3n@w3news.org>
2021-09-08 08:43:06 +0200

    Combine with the old capitalizeFirstLetter
    cleaner methods, smaller scopes

--- src/string-helper.js
+++ src/string-helper.js
@@ -1,5 +1,8 @@
+export const capitalizeFirstLetter = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
+
 export const capitalizeWords = (string) =>
     string
         .split(/_| /)
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .map((word) => capitalizeFirstLetter(word))
         .join('');
